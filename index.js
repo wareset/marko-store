ce299089c7e8d3046dddbc5fb4bf82f1588e855f
wareset <git@wareset.com>
2020-09-06 08:05:35 +0500

    Fix check component

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const waresetStore = require('@wareset/store');
 
-const marko_Component = require('marko/src/runtime/components/Component');
-const marko_ServerComponent = require('marko/src/runtime/components/ServerComponent');
-
-// const updateManager = require('marko/src/runtime/components//update-manager');
-
-const COMPONENT_TYPES = [marko_Component, marko_ServerComponent];
-
 function check_component(components, component, args) {
-  // console.log(component);
-  if (!COMPONENT_TYPES.some(v => component instanceof v)) {
+  if (!component || !component.elId || component.elId !== component.getElId) {
     args.unshift(component);
     return 0;
   }
 
-  if (component instanceof marko_ServerComponent) return 1;
+  if (!component.setState) return 1;
   if (components.indexOf(component) < 0) return 2;
 
   return true;
